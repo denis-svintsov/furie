23564ae12c51c0af3b74f610112bade9c2db522e
dmitriy-svintsov <72872027+dmitriy-svintsov@users.noreply.github.com>
2020-12-05 09:36:25 +0400

    Add slider

--- js/script.js
+++ js/script.js
@@ -1,3 +1,15 @@
+//Иницилизация Swiper
+new Swiper('.image-slider', {
+            direction: 'vertical',
+            slidesPerView: 1,
+            spaceBetween: 30,
+            mousewheel: true,
+            pagination: {
+                el: '.swiper-pagination',
+                clickable: true,
+            },
+        });
+
 $(document).ready(function() {
 	$('.header__burger').click(function(event) {
 		$('.header__burger,.header__menu').toggleClass('active');
